d74505bde42d712d7d952ece567284f985e2ee6c
Chaerin Hwang <chaa00@naver.com>
2024-07-03 17:10:44 +0900

    feat: memberService 임시 조회 메서드 구현

--- src/main/java/sws/songpin/entity/member/repository/MemberRepository.java
+++ src/main/java/sws/songpin/entity/member/repository/MemberRepository.java
@@ -1,4 +1,7 @@
 package sws.songpin.entity.member.repository;
 
-public class MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import sws.songpin.entity.member.domain.Member;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
 }
