366a511412b4b55365ca6a86de735e8518ceedb9
seohyun-lee <applemint2003@gmail.com>
2024-07-04 14:03:56 +0900

    feat: Playlist에 Visibility 속성이 빠져 추가함

--- src/main/java/sws/songpin/entity/playlist/domain/Playlist.java
+++ src/main/java/sws/songpin/entity/playlist/domain/Playlist.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import sws.songpin.entity.bookmark.domain.Bookmark;
 import sws.songpin.entity.member.domain.Member;
-import sws.songpin.entity.pin.domain.Pin;
+import sws.songpin.entity.pin.domain.Visibility;
 import sws.songpin.entity.playlistPin.domain.PlaylistPin;
 import sws.songpin.global.BaseTimeEntity;
 
@@ -22,12 +22,16 @@ public class Playlist extends BaseTimeEntity {
     @Column(name = "playlist_id", updatable = false)
     private Long playlistId;
 
+    @Column(name = "playlist_name", nullable = false)
+    private String playlistName;
+
+    @Column(name = "visibility", length = 10, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Visibility visibility;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "creator_id", nullable = false, updatable = false)
     private Member creator;
-
-    @Column(name = "playlist_name", nullable = false)
-    private String playlistName;
 
     @OneToMany(mappedBy = "playlist", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PlaylistPin> playlistPins;
@@ -36,10 +40,12 @@ public class Playlist extends BaseTimeEntity {
     private List<Bookmark> bookmarks;
 
     @Builder
-    public Playlist(Long playlistId, Member creator, String playlistName) {
+    public Playlist(Long playlistId, String playlistName, Visibility visibility, Member creator) {
         this.playlistId = playlistId;
-        this.creator = creator;
         this.playlistName = playlistName;
+        this.visibility = visibility;
+        this.creator = creator;
         this.playlistPins = new ArrayList<>();
+        this.bookmarks = new ArrayList<>();
     }
 }
