439a44685ebb59494f03f60ffc7bf20add3f1c44
seohyun-lee <applemint2003@gmail.com>
2024-07-08 01:54:04 +0900

    fix: FollowController swagger tag, operation 수정

--- src/main/java/sws/songpin/domain/follow/controller/FollowController.java
+++ src/main/java/sws/songpin/domain/follow/controller/FollowController.java
@@ -1,5 +1,6 @@
 package sws.songpin.domain.follow.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -8,30 +9,31 @@ import org.springframework.web.bind.annotation.*;
 import sws.songpin.domain.follow.dto.request.FollowAddRequestDto;
 import sws.songpin.domain.follow.service.FollowService;
 
+@Tag(name = "Follow", description = "Follow 관련 API입니다.")
 @RestController
 @RequiredArgsConstructor
 public class FollowController {
     private final FollowService followService;
 
-    @Tag(name = "유저의 팔로잉 목록 조회", description = "유저의 팔로잉 목록을 불러옵니다.")
+    @Operation(summary = "유저의 팔로잉 목록 조회", description = "유저의 팔로잉 목록을 불러옵니다.")
     @GetMapping("/members/{memberId}/followings")
     public ResponseEntity<?> followingList(@PathVariable final Long memberId) {
         return ResponseEntity.ok(followService.getFollowingList(memberId));
     }
 
-    @Tag(name = "유저의 팔로워 목록 조회", description = "유저의 팔로워 목록을 불러옵니다.")
+    @Operation(summary = "유저의 팔로워 목록 조회", description = "유저의 팔로워 목록을 불러옵니다.")
     @GetMapping("/members/{memberId}/followers")
     public ResponseEntity<?> followerList(@PathVariable final Long memberId) {
         return ResponseEntity.ok(followService.getFollowerList(memberId));
     }
 
-    @Tag(name = "타 유저를 팔로잉", description = "다른 유저를 팔로잉합니다.")
+    @Operation(summary = "타 유저를 팔로잉", description = "다른 유저를 팔로잉합니다.")
     @PostMapping("/follows")
-    public ResponseEntity<?> followAdd(@Valid @RequestBody FollowAddRequestDto followAddRequestDto) {
+    public ResponseEntity<?> followAdd(@RequestBody @Valid FollowAddRequestDto followAddRequestDto) {
         return ResponseEntity.ok(followService.addFollow(followAddRequestDto));
     }
 
-    @Tag(name = "팔로잉 취소 또는 팔로워 삭제", description = "나의 팔로잉을 취소하거나 팔로워를 삭제합니다.")
+    @Operation(summary = "팔로잉 취소 또는 팔로워 삭제", description = "나의 팔로잉을 취소하거나 팔로워를 삭제합니다.")
     @DeleteMapping("/follows/{followId}")
     public ResponseEntity<?> followRemove(@PathVariable final Long followId) {
         followService.deleteFollow(followId);
