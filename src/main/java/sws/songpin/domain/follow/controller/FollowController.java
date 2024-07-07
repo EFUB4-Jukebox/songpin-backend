667dc3f9f7d96233faca2b79665e73779d2e3f7f
seohyun-lee <applemint2003@gmail.com>
2024-07-08 01:43:15 +0900

    fix: FollowController에 애노테이션과 swagger tag 추가, FollowAddRequestDto에 @NotNull 제약조건 추가

--- src/main/java/sws/songpin/domain/follow/controller/FollowController.java
+++ src/main/java/sws/songpin/domain/follow/controller/FollowController.java
@@ -1,5 +1,7 @@
 package sws.songpin.domain.follow.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,21 +13,25 @@ import sws.songpin.domain.follow.service.FollowService;
 public class FollowController {
     private final FollowService followService;
 
+    @Tag(name = "유저의 팔로잉 목록 조회", description = "유저의 팔로잉 목록을 불러옵니다.")
     @GetMapping("/members/{memberId}/followings")
     public ResponseEntity<?> followingList(@PathVariable final Long memberId) {
         return ResponseEntity.ok(followService.getFollowingList(memberId));
     }
 
+    @Tag(name = "유저의 팔로워 목록 조회", description = "유저의 팔로워 목록을 불러옵니다.")
     @GetMapping("/members/{memberId}/followers")
     public ResponseEntity<?> followerList(@PathVariable final Long memberId) {
         return ResponseEntity.ok(followService.getFollowerList(memberId));
     }
 
+    @Tag(name = "타 유저를 팔로잉", description = "다른 유저를 팔로잉합니다.")
     @PostMapping("/follows")
-    public ResponseEntity<?> followAdd(FollowAddRequestDto followAddRequestDto) {
+    public ResponseEntity<?> followAdd(@Valid @RequestBody FollowAddRequestDto followAddRequestDto) {
         return ResponseEntity.ok(followService.addFollow(followAddRequestDto));
     }
 
+    @Tag(name = "팔로잉 취소 또는 팔로워 삭제", description = "나의 팔로잉을 취소하거나 팔로워를 삭제합니다.")
     @DeleteMapping("/follows/{followId}")
     public ResponseEntity<?> followRemove(@PathVariable final Long followId) {
         followService.deleteFollow(followId);
--- src/main/java/sws/songpin/domain/follow/dto/request/FollowAddRequestDto.java
+++ src/main/java/sws/songpin/domain/follow/dto/request/FollowAddRequestDto.java
@@ -1,11 +1,12 @@
 package sws.songpin.domain.follow.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import sws.songpin.domain.follow.entity.Follow;
 import sws.songpin.domain.member.entity.Member;
 
 public record FollowAddRequestDto(
-        Long followerId,
-        Long followingId
+        @NotNull Long followerId,
+        @NotNull Long followingId
 ){
     public static Follow toEntity(Member follower, Member following) {
         return Follow.builder()
