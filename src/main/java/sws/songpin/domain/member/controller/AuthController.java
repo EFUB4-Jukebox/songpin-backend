5d64b4cc76f4b83beb84e676676098928e8a51f9
Chaerin Hwang <chaa00@naver.com>
2024-07-08 14:31:06 +0900

    refactor: Member 도메인 클래스명 변경
    회원가입/로그인 등의 기능을 AuthController, AuthService 에서 처리하도록 변경

--- src/main/java/sws/songpin/domain/member/controller/AuthController.java
+++ src/main/java/sws/songpin/domain/member/controller/AuthController.java
@@ -13,24 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 import sws.songpin.domain.member.dto.request.LoginRequestDto;
 import sws.songpin.domain.member.dto.request.SignUpRequestDto;
 import sws.songpin.domain.member.dto.response.LoginResponseDto;
+import sws.songpin.domain.member.service.AuthService;
 import sws.songpin.domain.member.service.MemberService;
 
 @RestController
 @RequiredArgsConstructor
-public class LoginController {
-    private final MemberService memberService;
+public class AuthController {
+    private final AuthService authService;
 
     @Operation(summary = "회원가입", description = "회원 가입을 통해 유저 생성")
     @PostMapping("/signup")
     public ResponseEntity<Object> signUp(@Valid @RequestBody SignUpRequestDto requestDto){
-        memberService.signUp(requestDto);
+        authService.signUp(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @Operation(summary = "로그인", description = "로그인 결과를 반환합니다.")
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto requestDto){
-        LoginResponseDto responseDto = memberService.login(requestDto);
+        LoginResponseDto responseDto = authService.login(requestDto);
         return ResponseEntity.ok(responseDto);
     }
 
--- src/main/java/sws/songpin/domain/member/service/MemberService.java
+++ src/main/java/sws/songpin/domain/member/service/MemberService.java
@@ -23,9 +23,6 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtil jwtUtil;
 
     //개발 진행을 위한 임시 메서드
     public Member getCurrentMember(){
@@ -34,43 +31,6 @@ public class MemberService {
 
     public Member getMemberById(Long memberId){
         return memberRepository.findById(memberId).get();
-    }
-
-    public void signUp(SignUpRequestDto requestDto) {
-
-        //이메일 중복 검사
-        if (memberRepository.findByEmail(requestDto.email()).isPresent()) {
-            throw new CustomException(ErrorCode.EMAIL_ALREADY_EXISTS);
-        }
-
-        //비밀번호 일치 검사
-        if (!requestDto.password().equals(requestDto.confirmPassword())) {
-            throw new CustomException(ErrorCode.PASSWORD_MISMATCH);
-        }
-
-        //handle 랜덤값 생성
-        String handle = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 12);
-
-        //Member 객체 생성 후 저장
-        Member member = requestDto.toEntity(handle, passwordEncoder.encode(requestDto.password()));
-        memberRepository.save(member);
-    }
-
-    public LoginResponseDto login(LoginRequestDto requestDto){
-
-        try{
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            requestDto.email(), requestDto.password()
-                    )
-            );
-
-            LoginResponseDto responseDto = new LoginResponseDto(jwtUtil.generateAccessToken(authentication), jwtUtil.generateRefreshToken(authentication) );
-
-            return responseDto;
-        } catch (BadCredentialsException e){
-            throw new CustomException(ErrorCode.LOGIN_FAIL);
-        }
     }
 
 }
