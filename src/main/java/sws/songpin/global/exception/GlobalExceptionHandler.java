63e7615e014ee823b9c72c343c59c6355819d514
seohyun-lee <applemint2003@gmail.com>
2024-07-14 03:13:32 +0900

    refactor: 불필요한 주석, 사용하지 않는 코드 삭제

--- src/main/java/sws/songpin/global/exception/GlobalExceptionHandler.java
+++ src/main/java/sws/songpin/global/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package sws.songpin.global.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
