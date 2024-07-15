9fbcabad4a15547aab5088fdbecae5eade2ab1e7
judith <8w8u8@naver.com>
2024-07-16 03:28:57 +0900

    refactor: 마이페이지 컨트롤러 생성

--- src/main/java/sws/songpin/domain/member/controller/MemberController.java
+++ src/main/java/sws/songpin/domain/member/controller/MemberController.java
@@ -6,11 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sws.songpin.domain.member.service.MemberService;
+import sws.songpin.domain.playlist.service.PlaylistService;
 
 @Tag(name = "Member", description = "Member 관련 API입니다.")
 @RestController
@@ -18,11 +16,18 @@ import sws.songpin.domain.member.service.MemberService;
 @RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
+    private final PlaylistService playlistService;
 
     @Operation(summary = "유저 검색", description = "유저를 검색합니다.")
     @GetMapping
     public ResponseEntity<?> searchMembers(@RequestParam final String keyword,
                                            @PageableDefault(size = 20) final Pageable pageable) {
         return ResponseEntity.ok().body(memberService.searchMembers(keyword, pageable));
+    }
+
+    @Operation(summary = "타 유저 플레이리스트 목록 조회", description = "타 유저 페이지에서 플레이리스트 목록 조회")
+    @GetMapping("/members/{memberId}/playlists")
+    public ResponseEntity<?> getAllPlaylists(@PathVariable("memberId") final Long memberId){
+        return ResponseEntity.ok(playlistService.getAllPlaylists(memberId));
     }
 }
