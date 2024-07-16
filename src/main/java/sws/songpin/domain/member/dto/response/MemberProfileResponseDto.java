d681acd81692ef4ca57612ee54f4302672e38528
Chaerin Hwang <chaa00@naver.com>
2024-07-17 01:45:59 +0900

    refactor: dto 이름 변경
    MemberResponseDto → MemberProfileResponseDto

---
+++ src/main/java/sws/songpin/domain/member/dto/response/MemberProfileResponseDto.java
@@ -0,0 +1,17 @@
+package sws.songpin.domain.member.dto.response;
+
+import sws.songpin.domain.member.entity.Member;
+import sws.songpin.domain.member.entity.ProfileImg;
+
+public record MemberProfileResponseDto(
+        ProfileImg profileImg,
+        String nickname,
+        String handle,
+        long follower,
+        long following,
+        Long followId
+) {
+    public static MemberProfileResponseDto from(Member member, long followerCount, long followingCount, Long followId){
+        return new MemberProfileResponseDto(member.getProfileImg(), member.getNickname(), member.getHandle(), followerCount, followingCount, followId);
+    }
+}
