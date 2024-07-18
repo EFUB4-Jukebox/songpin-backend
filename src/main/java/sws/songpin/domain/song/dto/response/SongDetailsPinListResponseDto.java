e8c45544ba1cf9885981970bfbe4ce10ab5955fb
judith <8w8u8@naver.com>
2024-07-18 13:47:37 +0900

    fix: SongDetailsPinListResponseDto에서 pinCount 제거(songDetailsReponseDto로 옮김)

--- src/main/java/sws/songpin/domain/song/dto/response/SongDetailsPinListResponseDto.java
+++ src/main/java/sws/songpin/domain/song/dto/response/SongDetailsPinListResponseDto.java
@@ -3,12 +3,10 @@ package sws.songpin.domain.song.dto.response;
 import java.util.List;
 
 public record SongDetailsPinListResponseDto(
-    int pinCount,
     List<SongDetailsPinDto> songDetailsPinList){
 
     public static SongDetailsPinListResponseDto from(List<SongDetailsPinDto> songDetailsPinList) {
         return new SongDetailsPinListResponseDto(
-                songDetailsPinList.size(),
                 songDetailsPinList
         );
     }
