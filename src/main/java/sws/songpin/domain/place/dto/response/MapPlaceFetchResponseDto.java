c422c6483dd8973f8d4ec435e9ad9131676f730a
seohyun-lee <applemint2003@gmail.com>
2024-07-21 02:57:42 +0900

    fix: 매필 변수 오류 수정

--- src/main/java/sws/songpin/domain/place/dto/response/MapPlaceFetchResponseDto.java
+++ src/main/java/sws/songpin/domain/place/dto/response/MapPlaceFetchResponseDto.java
@@ -1,21 +1,21 @@
 package sws.songpin.domain.place.dto.response;
 
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
 
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public record MapPlaceFetchResponseDto(
-        Long mapPlaceCount,
+        int mapPlaceCount,
         Set<MapPlaceUnitDto> mapPlaceSet
 ){
-    public static MapPlaceFetchResponseDto from(Page<MapPlaceProjectionDto> dtoPage) {
-        Set<MapPlaceUnitDto> mapPlaceSet = dtoPage.stream()
+    public static MapPlaceFetchResponseDto from(Slice<MapPlaceProjectionDto> dtoSlice) {
+        Set<MapPlaceUnitDto> mapPlaceSet = dtoSlice.stream()
                 .map(MapPlaceUnitDto::from)
                 .collect(Collectors.toCollection(HashSet::new));
         return new MapPlaceFetchResponseDto(
-                dtoPage.getTotalElements(),
+                mapPlaceSet.size(),
                 mapPlaceSet
         );
     }
