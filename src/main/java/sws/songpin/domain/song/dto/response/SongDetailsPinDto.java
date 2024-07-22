2b2c2f115e811a186871ea95f4ab33ce246bc717
judith <8w8u8@naver.com>
2024-07-23 00:04:42 +0900

    feat: 핀에 멤버정보/좌표정보 추가

--- src/main/java/sws/songpin/domain/song/dto/response/SongDetailsPinDto.java
+++ src/main/java/sws/songpin/domain/song/dto/response/SongDetailsPinDto.java
@@ -7,21 +7,27 @@ import java.time.LocalDate;
 
 public record SongDetailsPinDto(
         Long pinId,
+        Long creatorId,
         String creatorNickname,
         LocalDate listenedDate,
         String memo,
         Visibility visibility,
         String placeName,
+        double latitude,
+        double longitude,
         Boolean isMine) {
 
     public static SongDetailsPinDto from(Pin pin, Boolean isMine) {
         return new SongDetailsPinDto(
                 pin.getPinId(),
+                pin.getMember().getMemberId(),
                 pin.getMember().getNickname(),
                 pin.getListenedDate(),
                 pin.getMemo(),
                 pin.getVisibility(),
                 pin.getPlace().getPlaceName(),
+                pin.getPlace().getLatitude( ),
+                pin.getPlace().getLongitude(),
                 isMine
         );
     }
