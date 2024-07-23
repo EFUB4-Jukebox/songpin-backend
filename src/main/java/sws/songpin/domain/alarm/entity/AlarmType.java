651472731ee95c63d5343d10035535c1cd4b079b
seohyun-lee <applemint2003@gmail.com>
2024-07-23 15:14:19 +0900

    style: AlarmType 메시지 문구 변경

--- src/main/java/sws/songpin/domain/alarm/entity/AlarmType.java
+++ src/main/java/sws/songpin/domain/alarm/entity/AlarmType.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum AlarmType {
-    FOLLOW("{0}(@{1})님이 팔로우했습니다."),
-    DEFAULT("{0}(@{1})님이 보낸 알림입니다.")
+    FOLLOW("{0}(@{1})님이 팔로우했어요."),
+    DEFAULT("{0}(@{1})님이 보낸 알림이에요.")
     ;
 
     private final String messagePattern;
