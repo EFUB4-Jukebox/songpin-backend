91af62fe8da652760f8c5788075c2edd40068ab0
seohyun-lee <applemint2003@gmail.com>
2024-07-23 19:35:22 +0900

    refactor: ErrorCode명과 메시지 직관적으로 변경

--- src/main/java/sws/songpin/global/exception/ErrorCode.java
+++ src/main/java/sws/songpin/global/exception/ErrorCode.java
@@ -58,8 +58,8 @@ public enum ErrorCode {
     EMAIL_ALREADY_EXISTS(409, "이미 가입된 이메일입니다."),
     FOLLOW_ALREADY_EXISTS(409, "이미 팔로우하고 있습니다."),
     BOOKMARK_ALREADY_EXISTS(409, "이미 북마크가 되어있습니다."),
-    PIN_ALREADY_EXISTS(409, "이미 플레이리스트에 추가된 핀입니다."),
-    HANDLE_ALREADY_EXISTS(409,"이미 존재하는 핸들입니다."),
+    PLAYLIST_PIN_ALREADY_EXISTS(409, "이미 플레이리스트에 추가된 핀입니다."),
+    HANDLE_ALREADY_EXISTS(409,"이미 사용 중인 핸들입니다."),
 
     // 500 Internal Server Error
     // 외부 API 사용 도중 에러
