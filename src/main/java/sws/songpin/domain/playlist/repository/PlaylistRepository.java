8bdac08ff48803e1197383e1ba79db2faff7e1d0
gkdudans <chrismhy001@gmail.com>
2024-07-23 16:12:49 +0900

    feat: Playlist 검색에 필터링 추가

--- src/main/java/sws/songpin/domain/playlist/repository/PlaylistRepository.java
+++ src/main/java/sws/songpin/domain/playlist/repository/PlaylistRepository.java
@@ -13,43 +13,48 @@ import java.util.List;
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
     List<Playlist> findAllByCreator(Member member);
 
-
     @Query(value = "SELECT p.playlist_id, p.playlist_name, COUNT(pin.pin_id) AS pin_count " +
             "FROM playlist p " +
             "LEFT JOIN playlist_pin pin ON p.playlist_id = pin.playlist_id " +
             "WHERE REPLACE(p.playlist_name, ' ', '') LIKE %:keywordNoSpaces% " +
+            "AND (p.visibility = 'PUBLIC' OR p.creator_id = :currentMemberId) " +
             "GROUP BY p.playlist_id " +
             "ORDER BY pin_count DESC, p.playlist_name ASC",
             countQuery = "SELECT COUNT(DISTINCT p.playlist_id) " +
                     "FROM playlist p " +
                     "LEFT JOIN playlist_pin pin ON p.playlist_id = pin.playlist_id " +
-                    "WHERE REPLACE(p.playlist_name, ' ', '') LIKE %:keywordNoSpaces%",
+                    "WHERE REPLACE(p.playlist_name, ' ', '') LIKE %:keywordNoSpaces% " +
+                    "AND (p.visibility = 'PUBLIC' OR p.creator_id = :currentMemberId)",
             nativeQuery = true)
-    Page<Object[]> findAllByPlaylistNameContainingIgnoreSpacesOrderByCount(@Param("keywordNoSpaces") String keywordNoSpaces, Pageable pageable);
+    Page<Object[]> findAllByPlaylistNameContainingIgnoreSpacesOrderByCount(@Param("keywordNoSpaces") String keywordNoSpaces, @Param("currentMemberId") Long currentMemberId, Pageable pageable);
 
     @Query(value = "SELECT p.playlist_id, p.playlist_name, COUNT(pin.pin_id) AS pin_count " +
             "FROM playlist p " +
             "LEFT JOIN playlist_pin pin ON p.playlist_id = pin.playlist_id " +
             "WHERE REPLACE(p.playlist_name, ' ', '') LIKE %:keywordNoSpaces% " +
+            "AND (p.visibility = 'PUBLIC' OR p.creator_id = :currentMemberId) " +
             "GROUP BY p.playlist_id " +
             "ORDER BY MAX(p.modified_time) DESC",
             countQuery = "SELECT COUNT(DISTINCT p.playlist_id) " +
                     "FROM playlist p " +
                     "LEFT JOIN playlist_pin pin ON p.playlist_id = pin.playlist_id " +
-                    "WHERE REPLACE(p.playlist_name, ' ', '') LIKE %:keywordNoSpaces%",
+                    "WHERE REPLACE(p.playlist_name, ' ', '') LIKE %:keywordNoSpaces% " +
+                    "AND (p.visibility = 'PUBLIC' OR p.creator_id = :currentMemberId)",
             nativeQuery = true)
-    Page<Object[]> findAllByPlaylistNameContainingIgnoreSpacesOrderByNewest(@Param("keywordNoSpaces") String keywordNoSpaces, Pageable pageable);
+    Page<Object[]> findAllByPlaylistNameContainingIgnoreSpacesOrderByNewest(@Param("keywordNoSpaces") String keywordNoSpaces, @Param("currentMemberId") Long currentMemberId, Pageable pageable);
 
     @Query(value = "SELECT p.playlist_id, p.playlist_name, COUNT(pin.pin_id) AS pin_count " +
             "FROM playlist p " +
             "LEFT JOIN playlist_pin pin ON p.playlist_id = pin.playlist_id " +
             "WHERE REPLACE(p.playlist_name, ' ', '') LIKE %:keywordNoSpaces% " +
+            "AND (p.visibility = 'PUBLIC' OR p.creator_id = :currentMemberId) " +
             "GROUP BY p.playlist_id " +
             "ORDER BY p.playlist_name ASC",
             countQuery = "SELECT COUNT(DISTINCT p.playlist_id) " +
                     "FROM playlist p " +
                     "LEFT JOIN playlist_pin pin ON p.playlist_id = pin.playlist_id " +
-                    "WHERE REPLACE(p.playlist_name, ' ', '') LIKE %:keywordNoSpaces%",
+                    "WHERE REPLACE(p.playlist_name, ' ', '') LIKE %:keywordNoSpaces% " +
+                    "AND (p.visibility = 'PUBLIC' OR p.creator_id = :currentMemberId)",
             nativeQuery = true)
-    Page<Object[]> findAllByPlaylistNameContainingIgnoreSpacesOrderByAccuracy(@Param("keywordNoSpaces") String keywordNoSpaces, Pageable pageable);
+    Page<Object[]> findAllByPlaylistNameContainingIgnoreSpacesOrderByAccuracy(@Param("keywordNoSpaces") String keywordNoSpaces, @Param("currentMemberId") Long currentMemberId, Pageable pageable);
 }
