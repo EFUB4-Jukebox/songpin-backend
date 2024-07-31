68eebb348fdf190b335c217b1cd9fc8874abac60
Chaerin Hwang <chaa00@naver.com>
2024-08-01 01:40:46 +0900

    refactor: 에러 메시지 수정

--- src/main/java/sws/songpin/domain/member/dto/request/ProfileUpdateRequestDto.java
+++ src/main/java/sws/songpin/domain/member/dto/request/ProfileUpdateRequestDto.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size;
 public record ProfileUpdateRequestDto (
         @NotBlank
         String profileImg,
-        @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "INVALID_INPUT_FORMAT-닉네임은 한글 문자, 영어 대소문자, 숫자만 허용됩니다. 공백은 허용되지 않습니다.")
+        @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "INVALID_INPUT_FORMAT-닉네임은 한글 문자, 영어 대소문자, 숫자만 허용됩니다.")
         @Size(max = 8, message = "INVALID_INPUT_LENGTH-닉네임은 8자 이내여야 합니다.")
         @NotBlank
         String nickname,
--- src/main/java/sws/songpin/domain/member/dto/request/SignUpRequestDto.java
+++ src/main/java/sws/songpin/domain/member/dto/request/SignUpRequestDto.java
@@ -7,7 +7,7 @@ public record SignUpRequestDto(
         @Email(message = "INVALID_INPUT_FORMAT-유효한 이메일 형식이 아닙니다.")
         @Size(max = 50, message = "INVALID_INPUT_LENGTH-이메일은 50자 이내여야 합니다.")
         String email,
-        @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "INVALID_INPUT_FORMAT-닉네임은 한글 문자, 영어 대소문자, 숫자만 허용됩니다. 공백은 허용되지 않습니다.")
+        @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "INVALID_INPUT_FORMAT-닉네임은 한글 문자, 영어 대소문자, 숫자만 허용됩니다.")
         @Size(max = 8, message = "INVALID_INPUT_LENGTH-닉네임은 8자 이내여야 합니다.")
         String nickname,
         @Size(min = 8, max = 20, message = "INVALID_INPUT_LENGTH-비밀번호는 최소 8자 이상, 20자 이내만 허용됩니다.")
