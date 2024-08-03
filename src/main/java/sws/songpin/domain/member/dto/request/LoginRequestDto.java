89ab9b5d84e396976f082e25db8c6ee95910366c
seohyun-lee <applemint2003@gmail.com>
2024-08-03 17:25:53 +0900

    fix: LoginRequestDto에서 @Email검사 삭제

--- src/main/java/sws/songpin/domain/member/dto/request/LoginRequestDto.java
+++ src/main/java/sws/songpin/domain/member/dto/request/LoginRequestDto.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
 public record LoginRequestDto(
-        @Email(message = "INVALID_INPUT_FORMAT-유효한 이메일 형식이 아닙니다.")
         @NotEmpty(message = "INVALID_INPUT_VALUE-이메일을 입력하세요.")
         String email,
         @NotEmpty(message = "INVALID_INPUT_VALUE-비밀번호를 입력하세요.")
