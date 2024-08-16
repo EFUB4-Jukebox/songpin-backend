2f85a27597f4fa5ba5df901559a8b9151d1db0f2
seohyun-lee <applemint2003@gmail.com>
2024-08-16 21:23:22 +0900

    refactor: 에러코드가 순서에 맞게 응답되도록 ObjectMapper를 사용

--- src/main/java/sws/songpin/global/config/AppConfig.java
+++ src/main/java/sws/songpin/global/config/AppConfig.java
@@ -1,7 +1,10 @@
 package sws.songpin.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,5 +13,12 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+        return objectMapper;
     }
 }
