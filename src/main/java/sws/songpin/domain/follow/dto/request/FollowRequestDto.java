9102851474c05653137090cbcfdb44d495514b03
seohyun-lee <applemint2003@gmail.com>
2024-08-16 23:08:58 +0900

    feat: 알림 API에서도 handle 사용하도록 변경

--- src/main/java/sws/songpin/domain/follow/dto/request/FollowRequestDto.java
+++ src/main/java/sws/songpin/domain/follow/dto/request/FollowRequestDto.java
@@ -5,7 +5,7 @@ import sws.songpin.domain.follow.entity.Follow;
 import sws.songpin.domain.member.entity.Member;
 
 public record FollowRequestDto(
-        @NotNull Long memberId
+        @NotNull String handle
 ){
     public static Follow toEntity(Member follower, Member following) {
         return Follow.builder()
--- src/main/java/sws/songpin/domain/member/dto/response/MyProfileResponseDto.java
+++ src/main/java/sws/songpin/domain/member/dto/response/MyProfileResponseDto.java
@@ -4,7 +4,6 @@ import sws.songpin.domain.member.entity.Member;
 import sws.songpin.domain.member.entity.ProfileImg;
 
 public record MyProfileResponseDto(
-        Long memberId,
         ProfileImg profileImg,
         String nickname,
         String handle,
@@ -13,6 +12,6 @@ public record MyProfileResponseDto(
         long followingCount
 ) {
     public static MyProfileResponseDto from(Member member, long followerCount, long followingCount){
-        return new MyProfileResponseDto(member.getMemberId(), member.getProfileImg(), member.getNickname(), member.getHandle(), member.getEmail(), followerCount, followingCount);
+        return new MyProfileResponseDto(member.getProfileImg(), member.getNickname(), member.getHandle(), member.getEmail(), followerCount, followingCount);
     }
 }
