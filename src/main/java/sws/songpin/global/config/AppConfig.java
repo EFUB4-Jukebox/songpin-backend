ebb614dd4dd4a95b676ed39a932064ee46d95f21
seohyun-lee <applemint2003@gmail.com>
2024-08-18 00:28:04 +0900

    hotfix: 날짜와 시간을 배열 대신 문자열로 직렬화

--- src/main/java/sws/songpin/global/config/AppConfig.java
+++ src/main/java/sws/songpin/global/config/AppConfig.java
@@ -18,6 +18,9 @@ public class AppConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
+        // 날짜와 시간을 배열 대신 문자열로 직렬화
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        // Map의 키를 정렬하는 설정 유지
         objectMapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
